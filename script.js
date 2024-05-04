32d13b535a9b326153af902d8b9a3785fb6feb2b
maddie0202 <148143460+maddie0202@users.noreply.github.com>
2024-05-04 20:30:39 +0330

    fixed some problems

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const display = document.getElementById("display");
+const previous = document.getElementById("previous");
+let storage = "";
 
 function append(input) {
+  if ("+-*/".includes(input) && "+-*/".includes(display.value.slice(-1))) {
+    return;
+  }
+  if (input === "." && display.value.includes(".")) {
+    return;
+  }
+
   display.value += input;
 }
 
 function clearDisplay() {
   display.value = "";
 }
-
-function performCalculation() {
-  display.value = eval(display.value);
-}
 function sign() {
   display.value = display.value * -1;
+}
+function percent() {
+  display.value = display.value / 100;
+}
+function performCalculation() {
+  display.value = eval(display.value);
 }
